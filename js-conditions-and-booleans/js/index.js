d90003408c765db05929baf06414a06437a19527
Marte Auer <marte_auer@hotmail.com>
2023-03-09 12:59:56 +0100

    add innerHTML styles

--- js-conditions-and-booleans/js/index.js
+++ js-conditions-and-booleans/js/index.js
@@ -3,16 +3,13 @@ const button = document.querySelector('[data-js="age-button"]');
 const output = document.querySelector('[data-js="age-output"]');
 
 button.addEventListener("click", () => {
-  // Number(x) converts the string from the input to a number
-  // Beware: input values are *always* strings!
     const age = Number(input.value);
 
-  // Exercise:
-  // Use conditions and the logical AND operator to write
-  // "You are a teen." or "You are not a teen." into the output.
-    if (age < 18) {
-        output.innerText="You are a teen."
-    } else {
-        output.innerText="You are not a teen."
-    }
+    age < 18 ? output.innerHTML="<h4><style>h4{color:red;font-size:60px;}</style> You are a teen!</h4>" : output.innerText="You are not a teen."
+
+    // if (age < 18) {
+    //     output.innerText="You are a teen."
+    // } else {
+    //     output.innerText="You are not a teen."
+    // }
 });

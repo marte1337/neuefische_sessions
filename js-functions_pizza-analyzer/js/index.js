cf33b4a041f236c15e40b23798214d152060e3b8
Marte Auer <marte_auer@hotmail.com>
2023-03-13 10:12:53 +0100

    add pizza js

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -1,48 +1,67 @@
 console.clear();
 
+// SELECTOR-SECTION: Pizza, Input-Filed, Output-Section
+
 const pizzaInput1 = document.querySelector('[data-js="pizza-input-1"]');
 const pizza1 = document.querySelector('[data-js="pizza-1"]');
 const pizzaInput2 = document.querySelector('[data-js="pizza-input-2"]');
 const pizza2 = document.querySelector('[data-js="pizza-2"]');
-const outputSection = document.querySelector('[data-js="output-section"]');
 
+const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
+
+// EVENT-LISTENER @ Input-Field:
+// Feeds Functions calaculatePizzaGain & updatePizzaDisplay
+// Refers to Selector-Section
+
 pizzaInput1.addEventListener("input", () => {
-  const pizzaSize1 = pizzaInput1.value
-  const pizzaSize2 = pizzaInput2.value
-  calculatePizzaGain(pizzaSize1, pizzaSize2)
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1, pizzaSize1);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  const pizzaSize1 = pizzaInput1.value
-  const pizzaSize2 = pizzaInput2.value
-  calculatePizzaGain(pizzaSize1, pizzaSize2)
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2, pizzaSize2);
 });
 
-// Task 1
+
+
+// FUNCTIONS
+
+
+// Calculate gain in Pizza (params from eventlisteners)
+// Output to text-output + updateOutputColor function
 
 function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = 2 * Math.PI * diameter1;
+  const area2 = 2 * Math.PI * diameter2;
+  const pizzaGain = (area2 - area1) / area1 * 100
 
- const area1 = 2 * Math.PI * diameter1;
- const area2 = 2 * Math.PI * diameter2;
+  output.textContent = Math.round(pizzaGain)
+  updateOutputColor(pizzaGain);
+ }
 
- const pizzaGain = (area2 - area1) / area1 * 100
 
-output.textContent = Math.round(pizzaGain)
+ // Change pizza dipslay sizes (params from eventlisteners + selector-section)
  
+ function updatePizzaDisplay(pizzaElement, newSize) {
+   pizzaElement.style.width = `${(newSize / 24) * 100}px`
+ }
 
-}
 
+// Change the Output Background (params from calculatePizzaGain-Function)
 
-// Task 2
-
-function updatePizzaDisplay(pizzaElement, newSize) {
-  // write your code here
-}
-
-// Task 3
-
-function updateOutputColor(size1, size2) {
-  // write your code here
+function updateOutputColor(pizzaGain) {
+  if (pizzaGain < 0) {
+    outputSection.style.backgroundColor = "var(--red)";
+  } else {
+    outputSection.style.backgroundColor = "var(--green)";
+  }
 }

197ed5ba87fce67394ac0617c87a1e6696a6b463
Marte Auer <marte_auer@hotmail.com>
2023-03-14 15:58:47 +0100

    add js

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const formData = new FormData (e.target)
+    const data = Object.fromEntries (formData)
+
+    console.log(data)
+
+    // Bonus: Calculate and log the age-badness-sum
+    console.log(
+        `The age-badness-sum of ${e.target.elements.firstName.value} is ${+e.target.elements.age.value + +e.target.elements.badness.value}`
+    )
+})

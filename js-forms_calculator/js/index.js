1de1baf88ba3b93e27dc5272478929a0143e2fe2
Marte Auer <marte_auer@hotmail.com>
2023-03-15 10:36:06 +0100

    add new js

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -26,6 +26,67 @@ form.addEventListener("submit", (event) => {
 
   // --v-- write your code here --v--
 
+// const numberA = event.target.elements.numberA.value
+// const numberB = event.target.elements.numberB.value
+// const operator = event.target.elements.operator.value
+
+// if (operator == "addition") {
+//   result = add(+numberA, +numberB)
+// } else if (operator == "subtraction") {
+//   result = subtract(+numberA, +numberB)
+// } else if (operator == "multiplication") {
+//   result = multiply(+numberA, +numberB)
+// } else {
+//   result = divide(+numberA, +numberB)
+// }
+
+
+// result = (operator, numberA, numberB) => {
+//   return add(+numberA, +numberB)
+//   ? (operator === "addition")
+//   : subtract(+numberA, +numberB)
+//   ? (operator === "subtraction")
+//   : multiply(+numberA, +numberB)
+//   ? (operator === "multiplication")
+//   : divide(+numberA, +numberB)
+//   }
+// }
+
+
+const formData = new FormData(event.target);
+const data = Object.fromEntries(formData);
+
+if (data.operator === "addition") {
+  result = add(+data.numberA, +data.numberB);
+} else if (data.operator === "subtraction") {
+  result = subtract(+data.numberA, +data.numberB);
+} else if (data.operator === "multiplication") {
+  result = multiply(+data.numberA, +data.numberB);
+} else if (data.operator === "division") {
+  result = divide(+data.numberA, +data.numberB);
+}
+
+
+// const numA = +data.numberA;
+// const numB = +data.numberB;
+
+// switch (data.operator) {
+//   case "addition":
+//     result = add(numA, numB);
+//     break;
+//   case "subtraction":
+//     result = subtract(numA, numB);
+//     break;
+//   case "multiplication":
+//     result = multiply(numA, numB);
+//     break;
+//   case "division":
+//     result = divide(numA, numB);
+//     break;
+//   default:
+//     break;
+// }
+
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -2,10 +2,13 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
+  success.setAttribute("hidden", "");
 }
+hideTosError();
 
 function showTosError() {
   tosError.removeAttribute("hidden");
@@ -15,9 +18,20 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  // const formData = new FormData(event.target);
+  // const data = Object.fromEntries(formData);
+  // console.log(data);
+
+  const tosData = event.target.elements.tos.checked;
+  console.log(tosData);
+
+  if (tosData === !true) {
+    return showTosError();
+  }
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  else alert("Form submitted");
+  success.removeAttribute("hidden");
 });
